23ab382fccea6ce0f315f89af3b69196c65986ab
CGranzell <cgranzell@hotmail.com>
2025-04-23 11:35:14 +0200

    test

---
+++ remake.Server/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace remake.Server.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public required string UserName { get; set; }
+
+        public string UserEmail { get; set; }
+    }
+}
