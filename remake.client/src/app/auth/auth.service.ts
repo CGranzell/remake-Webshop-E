e06a8033a272be1851f74ae8ed3ef61e096de67c
CGranzell <cgranzell@hotmail.com>
2025-06-26 07:59:52 +0200

    display username when logged in

--- remake.client/src/app/auth/auth.service.ts
+++ remake.client/src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { BehaviorSubject, tap } from "rxjs";
 export class AuthService {
     private readonly apiUrl = 'https://localhost:5193/api/auth';
     private tokenKey = 'jwt_token';
+    
 
     isLoggedIn$ = new BehaviorSubject<boolean>(this.hasToken());
 
@@ -16,25 +17,32 @@ export class AuthService {
 
     login(username: string, password: string) {
         console.log(`auth.service called with username: ${username}`);
+        
 
         return this.http.post(`${this.apiUrl}/login`, {userName: username, password}, { responseType: 'text'}).pipe(
             tap(token => {
                 console.log(`Token received: ${token}`);
                 localStorage.setItem(this.tokenKey, token);
+                localStorage.setItem('username', username);
                 this.isLoggedIn$.next(true);
             })
         );
+
     }
 
     logout() {
         localStorage.removeItem(this.tokenKey);
+        localStorage.removeItem('username');
         this.isLoggedIn$.next(false);
+        
     }
 
     getToken(): string | null {
-        // console.log(`Retrieving token from localStorage: ${this.tokenKey}`);
-        
         return localStorage.getItem(this.tokenKey);
+    }
+
+    getUsername(): string | null {
+        return localStorage.getItem('username');
     }
 
     private hasToken(): boolean {
--- remake.client/src/app/shared/navbar/navbar.component.ts
+++ remake.client/src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from '../../auth/login/login.component';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,25 +11,44 @@ import { LoginComponent } from '../../auth/login/login.component';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  username: string | null = '';
+  isLoggedIn: boolean = false;
+
 
 constructor(public authService: AuthService, private modalService: NgbModal) { }
+
+ngOnInit(): void {
+  this.username = this.authService.getUsername();
+  this.authService.isLoggedIn$.subscribe(status => {
+    this.isLoggedIn = status;
+    if ( status) {
+      this.username = this.authService.getUsername();
+
+    } else {
+      this.username = '';
+    }
+  })
+}
 
 openLoginModal(): void {
   const modalRef = this.modalService.open(LoginComponent);
   modalRef.result.then(
-    () =>{}, // optional: när modal stängs normalt
+    () =>{
+      if(this.authService.getToken()) {
+        this.username = this.authService.getUsername();
+      }
+    }, //  när modal stängs normalt, hämta username
     () => {} // optional: när modal avbryts (ESC eller klick utanför)
   );
 }
 
 logout(): void {
   this.authService.logout();
+  this.username = null; 
   console.log('User logged out');
 }
 
-isLoggedIn(): boolean {
-  return !!this.authService.getToken();
-}
+
 }
 
 
